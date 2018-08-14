0ab39c4649ad0882ee41d1718f753cb2abf99d31
skillnett <r.nemyrka@gmail.com>
2018-08-14 16:31:54 +0300

    Initial

--- js/index.js
+++ js/index.js
@@ -1,8 +1,18 @@
+// jQuery(document).ready(function(){
+
+// 	/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+// particlesJS.load('particles-js', '/js/vendor/particles/particles.json', function() {
+//   console.log('callback - particles.js config loaded');
+// });
+
+// });
+
 jQuery(document).ready(function(){
 
 	/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
-particlesJS.load('particles-js', '/js/vendor/particles/particles.json', function() {
+particlesJS.load('particles-js', '/my-project/js/vendor/particles/particles.json', function() {
   console.log('callback - particles.js config loaded');
 });
 
 });
+
