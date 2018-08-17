1ab2a4bc257d71241c5ce03a79df6a56c555ed29
skillnett <r.nemyrka@gmail.com>
2018-08-17 19:43:10 +0300

    gh-pages

--- js/index.js
+++ js/index.js
@@ -1,18 +1,18 @@
 // // animate background
-// $(document).ready(function(){
+$(document).ready(function(){
 
-// 	particlesJS.load('particles-js', '/my-project/js/vendor/particles/particles.json', function() {
-//   		console.log('callback - particles.js config loaded');
-// 	});
+	particlesJS.load('particles-js', '/my-project/js/vendor/particles/particles.json', function() {
+  		console.log('callback - particles.js config loaded');
+	});
 
-// });
+});
 
-// // preloader
-// $(document).ready(function() {
-// 	$('.progress-bar').circularProgress({
-// 		line_width: 5,
-// 		percent: 25,
-// 	}).circularProgress('animate', 100, 1000);
+// preloader
+$(document).ready(function() {
+	$('.progress-bar').circularProgress({
+		line_width: 5,
+		percent: 25,
+	}).circularProgress('animate', 100, 1000);
 });
 
 $(window).on('load', function () {
