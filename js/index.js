213abad7175dc727e5c7026cb91cedb3a666546b
skillnett <r.nemyrka@gmail.com>
2018-08-17 19:40:26 +0300

    gh-pages

--- js/index.js
+++ js/index.js
@@ -1,21 +1,32 @@
-$(document).ready(function(){
+// // animate background
+// $(document).ready(function(){
 
-	particlesJS.load('particles-js', '/my-project/js/vendor/particles/particles.json', function() {
-  		console.log('callback - particles.js config loaded');
-	});
+// 	particlesJS.load('particles-js', '/my-project/js/vendor/particles/particles.json', function() {
+//   		console.log('callback - particles.js config loaded');
+// 	});
 
-});
+// });
 
-
-$(document).ready(function() {
-	$('.progress-bar').circularProgress({
-		line_width: 5,
-		percent: 25,
-	}).circularProgress('animate', 100, 1000);
+// // preloader
+// $(document).ready(function() {
+// 	$('.progress-bar').circularProgress({
+// 		line_width: 5,
+// 		percent: 25,
+// 	}).circularProgress('animate', 100, 1000);
 });
 
 $(window).on('load', function () {
    var $preloader = $('#preloader');
-   $preloader.delay(1800).fadeOut('slow');
+   $preloader.delay(2000).fadeOut('slow');
 });
+
+// pop-up form
+$(".userInput").focus(function () {
+        $(this).parent().addClass("focus");
+    }).blur(function () {
+        if ($(this).val() === '') {
+            $(this).parent().removeClass("focus");
+        }
+    })
+
 
