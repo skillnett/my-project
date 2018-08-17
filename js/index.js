3fbaed069d8f088c7b9bfdd35a59e247bf892ecb
skillnett <r.nemyrka@gmail.com>
2018-08-17 13:17:04 +0300

    Initial

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-jQuery(document).ready(function(){
+$(document).ready(function(){
 
 	particlesJS.load('particles-js', '/my-project/js/vendor/particles/particles.json', function() {
   		console.log('callback - particles.js config loaded');
@@ -6,3 +6,15 @@ jQuery(document).ready(function(){
 
 });
 
+
+$(document).ready(function() {
+	$('.progress-bar').circularProgress({
+		line_width: 5,
+		percent: 25,
+	}).circularProgress('animate', 100, 2000);
+});
+
+$(window).on('load', function () {
+   var $preloader = $('#preloader');
+   $preloader.delay(1800).fadeOut('slow');
+});
